44f74783949cd620ed71bfcedae70b1bbfe6ac9a
Michael Henke <github@codingmerc.com>
2012-07-12 11:01:16 -0700

    line count starts at 1 corrected milliseconds part of time code

--- vdr2chap.pl
+++ vdr2chap.pl
@@ -2,7 +2,7 @@
 
 my $nLine = 0;
 
-chap($nLine++, "00:00:00.000","Intro\n");
+chap(++$nLine, "00:00:00.000","Intro\n");
 
 my @in = <STDIN>;
 
@@ -11,7 +11,7 @@ foreach my $line (@in){
     my @split = split(/ /, $line);
     my $timecode = shift(@split);
     my $text = join(" ", @split);
-    chap($nLine++, $timecode, $text);
+    chap(++$nLine, $timecode, $text);
 }
 exit(0);
 
@@ -22,7 +22,7 @@ sub chap {
     
     if(@time[2]) {
         my ($s, $ms) = split (/\./, @time[2]);
-
+        $ms *= 10;
         printf("CHAPTER%02d=%02d:%02d:%02d.%03d\nCHAPTER%02dNAME=%s",$line,$time[0],$time[1],$s,$ms,$line,$title);
     }
 }
