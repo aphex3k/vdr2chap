f5aae164e8e7af49478304b1c934ccf6732207e0
Michael Henke <github@codingmerc.com>
2012-07-12 10:48:48 -0700

    initial commit

---
+++ vdr2chap.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+my $nLine = 0;
+
+chap($nLine++, "00:00:00.000","Intro\n");
+
+my @in = <STDIN>;
+
+foreach my $line (@in){
+    
+    my @split = split(/ /, $line);
+    my $timecode = shift(@split);
+    my $text = join(" ", @split);
+    chap($nLine++, $timecode, $text);
+}
+exit(0);
+
+sub chap {
+    my ($line, $timecode, $title) = @_;
+
+    my @time = split(/:/, $timecode);
+    
+    if(@time[2]) {
+        my ($s, $ms) = split (/\./, @time[2]);
+
+        printf("CHAPTER%02d=%02d:%02d:%02d.%03d\nCHAPTER%02dNAME=%s",$line,$time[0],$time[1],$s,$ms,$line,$title);
+    }
+}
